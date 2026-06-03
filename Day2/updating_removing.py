725945c842e726f4cf79e6c6dcecbe035832f659
Sarah-0714 <simplyas0714@gmail.com>
2026-06-03 20:08:32 +0530

    Add examples for updating and removing list items

---
+++ Day2/updating_removing.py
@@ -0,0 +1,11 @@
+UPDATING:
+citizens = ["Assu", "Sarah", "Suba"]
+citizens[1] = "Anbu"
+print(citizens)
+
+Removing:
+citizens = ["Assu", "Sarah", "Suba"]
+citizens.remove("Suba")
+print(citizens)
+citizens.pop(0)
+print(citizens)
