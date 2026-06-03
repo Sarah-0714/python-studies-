06a7c81df43fbe599d29b751e502ac892e3aed9d
Sarah-0714 <simplyas0714@gmail.com>
2026-06-03 19:44:58 +0530

    Add examples for set and tuple in set_tuple.py

---
+++ Day2/set_tuple.py
@@ -0,0 +1,7 @@
+SET EXAMPLE:
+citizens = {"ASSU", "SARAH", "SUBA"}
+print(citizens)
+
+TUPLE EXAMPLE:
+aadhaar_services = ("New Registration", "Update Details", "Download Aadhaar")
+print(aadhaar_services[0])
