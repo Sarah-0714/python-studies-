be844887cde1367836ec3f66d93fd126c0918241
Sarah <simplyas0714@gmail.com>
2026-06-04 17:39:05 +0530

    Add list_of_dictionaries.py with student data

---
+++ Day3/list_of_dictionaries.py
@@ -0,0 +1,8 @@
+students = [
+    {"name": "Sarah", "course": "Btech", "city": "Coimbatore"},
+    {"name": "Assu", "course": "M.Sc", "city": "Bangalore"},
+    {"name": "Suba", "course": "Btech", "city": "Goa"}
+]
+
+for student in students:
+    print(student)
